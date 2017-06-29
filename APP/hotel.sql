78d6a048e71bca91b7b18b5f4defe02ce5f1222d
KILLERMAX91 <mathieu.barata@gmail.com>
2017-06-29 16:01:30 +0200

    mise à jour

---
+++ APP/hotel.sql
@@ -0,0 +1,69 @@
+SET SQL_SAFE_UPDATES = 0;
+-- DATABASE hotel
+delete from utilisateur;
+delete from client;
+delete from role;
+delete from chambre;
+delete from etage;
+delete from type_chambre;
+delete from menu;
+delete from facture;
+SET SQL_SAFE_UPDATES = 1;
+
+
+INSERT INTO `role` (`id`, `type_role`) VALUES
+(1, 'Directeur de l’hôtel'),
+(2, 'Directeur du restaurant'),
+(3, 'Directeur d’hébergement'),
+(4, 'Chef de réception'),
+(5, 'Gouvernante générale'),
+(6, 'Chef de maintenance'),
+(7, 'Spa manager');
+
+INSERT INTO menu(id, nom_menu, prix) VALUES
+(1, 'menu 1', 12.65),
+(3, 'menu 2', 10.37),
+(2, 'menu 3', 32.83);
+
+
+-- password = 123456
+INSERT INTO `utilisateur` (`id`, `lastname`, login, `mail`, `firstname`, `password`, `id_role`) VALUES
+(1, 'St-Pierre', 'jst-pierre', 'JeanSt-Pierre@jourrapide.fr', 'Jean', '123456', 1),
+(2, 'St-paul', 'jst-paul', 'JeanSt-Pierre@paul.fr', 'Jean', '123456', 3),
+(3, 'Konovalova', 'ata-Konovalova', 'ata@Konovalova.ru', 'ata', '123456', 4),
+(4, 'cartman', 'eric-cartman', 'eric@cartman.fr', 'eric', '123456', 2),
+(5, 'Zoidberg ', 'john-zoidberg', 'john@zoidberg.fr', 'John', '123456', 5),
+(6, 'Rodríguez ', 'bender-rodríguez', 'bender@rodriguez.fr', 'Bender', '123456', 6),
+(7, 'marsh', 'randy-marsh', 'randy@marsh.com', 'randy', '123456', 7);
+
+INSERT INTO client(id, lastname, firstname, mail) VALUES
+(1, 'client_1', 'bob', 'bob@client_1.com'),
+(2, 'client_2', 'joe', 'joe@client_2.com'),
+(3, 'client_3', 'vladimir', 'vladimir@client_3.com');
+
+
+INSERT INTO etage(id, num_etage) VALUES
+(1, 0),
+(2, 1),
+(3, 2);
+
+INSERT INTO type_chambre(id, suite) VALUES
+(1, "lit simple"),
+(2, "lit double"),
+(3, "grand lit double"),
+(4, "deux lits"),
+(5, "suite"),
+(6, "suite royal");
+
+
+INSERT INTO chambre(id, nbr_place, id_etage, prix, id_type) VALUES
+(1, 2, 1, 250, 2),
+(2, 2, 1, 300, 3),
+(3, 2, 2, 400, 4),
+(4, 1, 3, 120, 1),
+(5, 2, 2, 650, 5),
+(6, 2, 1, 1050, 6),
+(7, 1, 2, 140, 1);
+
+INSERT INTO facture(date_debut, date_fin, facture_type, id_client, id_chambre) VALUES
+("2017-01-01", "2017-01-14", "en cours", 2, 6);
---
+++ APP/hotel_structure.sql
@@ -0,0 +1,75 @@
+CREATE TABLE role (
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	type_role VARCHAR(255) UNIQUE, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE utilisateur (
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	mail VARCHAR(255) UNIQUE, 
+	password VARCHAR(255), 
+	lastname VARCHAR(255), 
+	login VARCHAR(255) UNIQUE, 
+	firstname VARCHAR(255), 
+	id_role BIGINT, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE client(
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	mail VARCHAR(255) UNIQUE, 
+	lastname VARCHAR(255), 
+	firstname VARCHAR(255),
+	PRIMARY KEY (id)
+);
+
+create table etage(
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	num_etage int(11),
+	PRIMARY KEY (id)
+);
+
+create table type_chambre(
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	suite varchar(200) not null,
+	PRIMARY KEY (id)
+);
+
+create table chambre(
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	nbr_place int(11) not null,
+	prix float not null,
+	id_etage BIGINT, 
+	id_type BIGINT,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE facture(
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	date_debut DATETIME,
+	date_fin DATETIME, 
+	facture_type VARCHAR(255), 
+	id_client BIGINT,
+	id_chambre BIGINT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE menu(
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	nom_menu varchar(200) not null,
+	prix float not null,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE incident(
+	id BIGINT AUTO_INCREMENT NOT NULL, 
+	description longtext NOT NULL,
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE utilisateur ADD CONSTRAINT fk_utilisateur_id_role FOREIGN KEY (id_role) REFERENCES role (id);
+ALTER TABLE chambre ADD CONSTRAINT fk_chambre_id_type FOREIGN KEY (id_type) REFERENCES type_chambre (id);
+ALTER TABLE chambre ADD CONSTRAINT fk_chambre_id_etage FOREIGN KEY (id_etage) REFERENCES etage (id);
+ALTER TABLE facture ADD CONSTRAINT fk_facture_id_client FOREIGN KEY (id_client) REFERENCES client (id);
+ALTER TABLE facture ADD CONSTRAINT fk_facture_id_chambre FOREIGN KEY (id_chambre) REFERENCES chambre (id);
